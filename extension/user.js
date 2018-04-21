eeec26e643319fb36f557b9b769ebedec89cbbd8
Alexander Rodin <rodin.alexander@gmail.com>
2018-04-22 01:33:44 +0300

    Fix the token problem

--- extension/user.js
+++ extension/user.js
@@ -1,8 +1,9 @@
-const SERVICE_URL = 'https://d3bdc870.ngrok.io/process'
+const SERVICE_URL = 'https://ce0de1df.ngrok.io/process'
+const MAIL_THREAD_URL = 'https://e.mail.ru/api/v1/threads/thread?ajax_call=1&tarball=e.mail.ru-f-alpha-mail-64117-v.demidov-1523618897.tgz&offset=0&limit=50&htmlencoded=false&cache=false&api=1'
 
-const fetchMessage = async (msg) => {
-  const url = new URL('https://e.mail.ru/api/v1/threads/thread?ajax_call=1&tarball=e.mail.ru-f-alpha-mail-64117-v.demidov-1523618897.tgz&tab-time=1524320652&offset=0&limit=50&htmlencoded=false&cache=false&api=1')
-  url.searchParams.set('token', window.mailru_api_token)
+const fetchMessage = async (msg, token) => {
+  const url = new URL(MAIL_THREAD_URL)
+  url.searchParams.set('token', token)
   url.searchParams.set('id', msg.id)
   url.searchParams.set('ajax_call', '1')
   const email = document.getElementsByClassName('x-ph__menu__button__text x-ph__menu__button__text_auth')[0].innerHTML
@@ -13,7 +14,7 @@ const fetchMessage = async (msg) => {
 }
 
 const rankMessages = async () => {
-  alert(window.mailru_api_token) // shouln't be undefined!!! FIXME
+  const token = document.querySelector('html').outerHTML.match(/patron\.updateToken\("(.*)"\)/)[1]
 
   const messages = []
   for (let elem of document.getElementsByClassName('js-href b-datalist__item__link')) {
@@ -23,19 +24,22 @@ const rankMessages = async () => {
     })
   }
 
-  const bodies = await Promise.all(messages.map(fetchMessage))
+  const bodies = await Promise.all(messages.map(msg => fetchMessage(msg, token)))
   bodies.forEach((body, i) => {
-    window.console.log(body)
     messages[i].body = body.body.messages[0].body.text
   })
   console.log('Messages:')
+  console.log(messages)
 
   const requestData = {messages: {}}
   messages.forEach(msg => {
-    requestData[msg.id] = msg.body
+    requestData.messages[msg.id] = msg.body
   })
 
-  const response = (await fetch(SERVICE_URL, {
+  console.log('Request data')
+  console.log(requestData)
+
+  const response = await (await fetch(SERVICE_URL, {
     body: JSON.stringify(requestData),
     cache: 'no-cache',
     headers: {
@@ -44,7 +48,7 @@ const rankMessages = async () => {
     method: 'POST',
     mode: 'cors'
   })).json()
-  const results = response['results']
+  const results = response
   console.log('Results')
   console.log(results)
 }
