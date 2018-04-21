8d6081cb3dcc6d52bd692135207c871e7d09a4f5
AliakseiSemchankau <leshaxim@mail.ru>
2018-04-21 21:11:33 +0300

    add server

---
+++ flaskapp/server.py
@@ -0,0 +1,47 @@
+from flask import Flask
+from flask import request
+import flask
+import json
+import pprint
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+def get_rank(key, value):
+	return '{} + {}'.format(key, value)
+
+@app.route('/process', methods=['POST'])
+def process():
+	print(request)
+	json_data = request.get_json()
+	print(json_data)
+	json_data = json_data['messages']
+	print(json_data)
+	results = {}
+	for key, value in json_data.items():
+		rank = get_rank(key, value)
+		results[key] = rank
+	response = flask.jsonify({'results': results})
+	response.headers.add('Access-Control-Allow-Origin', '*')
+	return response
+
+from flask import request
+
+def shutdown_server():
+    func = request.environ.get('werkzeug.server.shutdown')
+    if func is None:
+        raise RuntimeError('Not running with the Werkzeug Server')
+    func()
+
+@app.route('/shutdown', methods=['POST'])
+def shutdown():
+	shutdown_server()
+	resp = flask.make_response()
+	resp.headers['Access-Control-Allow-Origin'] = '*'
+	return resp
+	return 'Server shutting down...'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8081)
